4a395060d90d2a5e8afa18687d60699aa6b9db95
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-02-01 22:27:23 +0700

    Sec03 | Mono Subscribe - refactor code

--- reactor/src/main/java/com/ngochung/sec03/MonoSubscribe.java
+++ reactor/src/main/java/com/ngochung/sec03/MonoSubscribe.java
@@ -1,17 +1,20 @@
 package com.ngochung.sec03;
 
+import com.ngochung.courseUtil.Utils;
 import reactor.core.publisher.Mono;
 
 public class MonoSubscribe {
   public static void main(String[] args) {
     // publisher
-    Mono<String> mono = Mono.just("football");
+    Mono<Integer> mono = Mono.just("football")
+            .map(String:: length)
+            .map(l -> l/0);
 
     // subscribe
     mono.subscribe(
-            item -> System.out.println(item),
-            err -> System.out.println(err.getMessage()),
-            () -> System.out.println("Completed")
+            Utils.onNext(),
+            Utils.onError(),
+            Utils.onComplete()
     );
   }
 }
